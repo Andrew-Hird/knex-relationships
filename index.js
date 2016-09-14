14d8caacce121fe3ae0e0c356b9b2614c4541cac
Andrew Hird <eda@mac-mini-1.local>
2016-09-14 16:40:34 +1200

    new user working

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', index.get)
 app.get('/profile/:id', index.getProfiles)
+app.post('/newUser', index.newUser)
 
 app.listen(PORT, function () {
   console.log('Listening on port', PORT)
--- routes/index.js
+++ routes/index.js
@@ -4,29 +4,64 @@ var knex = require('knex')(development)
 
 module.exports = {
   get: get,
-  getProfiles: getProfiles
+  getProfiles: getProfiles,
+  newUser: newUser
 }
 
-function get (req, res) {
+function get(req, res) {
   knex('users')
     .select()
     .then(function (users) {
-      res.render('index', { users: users })
+      res.render('index', {
+        users: users
+      })
     })
     .catch(function (err) {
       res.status(500).send('DATABASE ERROR: ' + err.message)
     })
 }
 
-function getProfiles (req, res) {
+function getProfiles(req, res) {
   knex('users')
-  .join('profiles', 'users.id', '=', 'profiles.user_id')
-  .select('users.id', 'users.name', 'profiles.profilePic as pic', 'users.email', 'profiles.URL')
-  .where('users.id', '=', req.params.id)
-  .then(function (users) {
-    res.render('profile', {user: users[0]})
-  })
-  .catch(function (err) {
-    res.status(500).send('DATABASE ERROR: ' + err.message)
-  })
+    .join('profiles', 'users.id', '=', 'profiles.user_id')
+    .select('users.id', 'users.name', 'profiles.profilePic as pic', 'users.email', 'profiles.URL')
+    .where('users.id', '=', req.params.id)
+    .then(function (users) {
+      res.render('profile', {
+        user: users[0]
+      })
+    })
+    .catch(function (err) {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+}
+
+function newUser(req, res) {
+  var userDetails = {
+    name: req.body.name,
+    email: req.body.email,
+    url: req.body.url,
+    profilePic: req.body.profilePic
+  }
+
+  knex('users')
+    .insert({
+      name: userDetails.name,
+      email: userDetails.email
+    })
+    .then(function (ids) {
+      return knex('profiles')
+      .insert({
+        user_id: ids[0],
+        URL: userDetails.url,
+        profilePic: userDetails.profilePic
+      })
+    })
+
+    .then(function (data) {
+      res.redirect('/')
+    })
+    .catch(function (err) {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
 }
