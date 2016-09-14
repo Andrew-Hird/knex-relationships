985ecb4a7eb82301f193852e73fac809d070879d
Andrew Hird <eda@mac-mini-1.local>
2016-09-14 15:48:03 +1200

    user story one working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var development = require('../knexfile').development
 var knex = require('knex')(development)
 
 module.exports = {
-  get: get
+  get: get,
+  getProfiles: getProfiles
 }
 
 function get (req, res) {
@@ -15,4 +16,17 @@ function get (req, res) {
     .catch(function (err) {
       res.status(500).send('DATABASE ERROR: ' + err.message)
     })
+}
+
+function getProfiles (req, res) {
+  knex('users')
+  .join('profiles', 'users.id', '=', 'profiles.user_id')
+  .select('users.id', 'users.name', 'profiles.profilePic as pic')
+  .where('users.id', '=', req.params.id)
+  .then(function (users) {
+    res.render('profile', {user: users[0]})
+  })
+  .catch(function (err) {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
 }
